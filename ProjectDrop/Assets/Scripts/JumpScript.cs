b33f2d1c9592c57e37d454e79891cd76ef62d2f4
Raphko <rap.koller@gmail.com>
2015-04-26 17:50:02 +0200

    Various

---
+++ ProjectDrop/Assets/Scripts/JumpScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpScript : MonoBehaviour {
+
+	private Animator animDrop;
+
+	// Use this for initialization
+	void Start () {
+		animDrop = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Space)){
+			animDrop.SetTrigger("Jump");
+		}
+	}
+}
---
+++ ProjectDrop/Assets/Scripts/SplashPlatformer2DUserControl.cs
@@ -0,0 +1,46 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets._2D
+{
+    [RequireComponent(typeof (PlatformerCharacter2D))]
+    public class Platformer2DUserControl : MonoBehaviour
+    {
+        private PlatformerCharacter2D m_Character;
+        private bool m_Jump;
+		public float startSpeed = 1;
+		public float speedUp = 0.01f;
+
+        private void Awake()
+        {
+            m_Character = GetComponent<PlatformerCharacter2D>();
+			//InvokeRepeating ("GetFaster", 0, 1);
+		
+        }
+	
+
+
+
+
+        private void Update()
+        {
+            if (!m_Jump)
+            {
+                // Read the jump input in Update so button presses aren't missed.
+                m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+            }
+        }
+
+
+        private void FixedUpdate()
+        {
+            // Read the inputs.
+            bool crouch = Input.GetKey(KeyCode.LeftControl);
+            // Pass all parameters to the character control script.
+			startSpeed = startSpeed + speedUp;
+			m_Character.Move(startSpeed, crouch, m_Jump);
+            m_Jump = false;
+        }
+    }
+}
