53ac2d3d9e7c3c5924e2c1da9ba57efc78355284
Raphko <rap.koller@gmail.com>
2015-04-26 13:51:17 +0200

    First Commit

---
+++ ProjectDrop/Assets/NewBehaviourScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public Light myLight;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
